8bb150095980b742a2af171f807369c46dd9d50b
Yevgen Golubenko <yevgen.golubenko@getinsured.com>
2016-01-29 16:20:14 -0800

    initial

---
+++ src/main/java/com/getinsured/ms/samplemaven/MsSampleMavenApplication.java
@@ -0,0 +1,12 @@
+package com.getinsured.ms.samplemaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSampleMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSampleMavenApplication.class, args);
+	}
+}
